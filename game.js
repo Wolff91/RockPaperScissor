6e2836fbce68b58c697f1489f8e61e90b876fb4e
Wolff91 <Peterwolff@live.dk>
2023-01-11 12:30:11 +0100

    pseudo code

---
+++ game.js
@@ -0,0 +1,14 @@
+// getComputerChoice
+// create function called getComputerChoice
+// the computer chooses randomly between three options: rock, paper or scissor
+
+// playerSelection
+// create a function called playerSelection
+// the player chooses between three options: rock, paper or scissor.
+
+// Compare getComputerChoice and playerSelection to find out who one the round.
+// rock beats scissor, scissor beats paper and paper beats rock. 
+// give the winner a point  adding +1 from the current score.
+// if both getComputerCHoice and playerSelection chose the same, it's a tie and no one is awarded a point.
+// First to reach 5 points wins. 
+// when the round is finished give the option to start over. Unless one have reached 5 points.

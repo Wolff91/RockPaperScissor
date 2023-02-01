6020263e78055e885e3830425354d580fc519138
Wolff91 <Peterwolff@live.dk>
2023-02-01 10:29:08 +0100

    playable version - added js and html

--- game.js
+++ game.js
@@ -1,14 +1,59 @@
-// getComputerChoice
-// create function called getComputerChoice
-// the computer chooses randomly between three options: rock, paper or scissor
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('input')
 
-// playerSelection
-// create a function called playerSelection
-// the player chooses between three options: rock, paper or scissor.
+//randomly generates computer entry
+function getComputerChoice() {
+    let computerPick = ['rock', 'paper', 'scissor']
+    return computerPick[Math.floor(Math.random() * computerPick.length)] 
+}
 
-// Compare getComputerChoice and playerSelection to find out who one the round.
-// rock beats scissor, scissor beats paper and paper beats rock. 
-// give the winner a point  adding +1 from the current score.
-// if both getComputerCHoice and playerSelection chose the same, it's a tie and no one is awarded a point.
-// First to reach 5 points wins. 
-// when the round is finished give the option to start over. Unless one have reached 5 points.
+//plays a round of the game
+
+function playRound(playerSelection) {
+    let computerPlay = getComputerChoice()
+    let result = ""
+
+//defining the rules of the game
+
+     if ((playerSelection == 'rock' && computerPlay == 'scissor') ||
+         (playerSelection == 'paper' && computerPlay == 'rock') ||
+         (playerSelection == 'scissor' && computerPlay == 'paper')) {
+
+         playerScore +=1
+         result = ('You win! ' + playerSelection + ' beats ' + computerPlay +
+                '<br>Player score: ' + playerScore + '<br>Computer score: ' + 
+                computerScore)
+
+            if (playerScore == 5) {
+                result += '<br>You won the game! Reload to page to play again'
+            }
+    }
+        else if (playerSelection == computerPlay) {
+            result = ('It\'s a tie! you both choose ' + playerSelection + 
+                 ' no one get\'s a point' + '<br>player score: ' + playerScore +
+                 '<br>computer score: ' + computerScore) 
+        }
+
+        else {
+            computerScore +=1
+            result = ('You loose! ' + computerPlay + ' beats ' +
+                     playerSelection + '<br>Player score: ' + playerScore + 
+                     '<br>Computer score: ' + computerScore)
+
+            if (computerScore == 5) {
+                result += '<br>You lost the game! Reload to page to play again'
+            }
+            
+        }
+
+   document.getElementById('result').innerHTML = result
+   return
+}
+
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
